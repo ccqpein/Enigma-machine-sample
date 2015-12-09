5feefc0fffc2e8d2d1cd5c28df86712c35b502c9
ccQpein <ccqpein@protonmail.com>
2015-12-09 17:40:54 -0500

    first version

--- Enigma.py
+++ Enigma.py
@@ -1,12 +1,30 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-from Rotors import Rotors
+from Hardware import Rotors, Reflector
 
 
-def main():
-    rotor1 = Rotors()
-    rotor2 = Rotors()
-    rotor3 = Rotors()
-    print(rotor1.encrypt("LOL"))
+def buildEnigma():
+    rotor1 = Rotors(1)
+    rotor2 = Rotors(2)
+    rotor3 = Rotors(3)
+    # todo: make the rotor making function
+    rotorChain = [rotor1, rotor2, rotor3]
+    reflector = Reflector("R")  # reflector can be thought a rotor
+    return(rotorChain, reflector)
+
+rotorChain, reflector = buildEnigma()
+
+
+def encryptProcess(string):
+    global rotorChain, reflector
+    for rotor in rotorChain:
+        string = rotor.encrypt(string)
+
+    string = reflector.encrypt(string)
+
+    for rotor in reversed(rotorChain):
+        string = rotor.encrypt(string)
+
+    print(string)
+    return string
---
+++ Hardware.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+originalKey = ["a", "b", "c", "d", "e", "f", "g",
+               "h", "i", "j", "k", "l", "m", "n",
+               "o", "p", "q", "r", "s", "t", "u",
+               "v", "w", "x", "y", "z"]
+
+
+class Rotors(object):
+
+    def __init__(self, *args):
+        self.originalDict = [i for i in originalKey]
+        random.shuffle(self.originalDict)
+        self.originalKey = []
+
+        for i in reversed(self.originalDict):
+            self.originalKey.append(i)
+
+        self.__rotorId__ = args[0]
+        # print(self.originalKey)
+        # print(self.originalDict)
+
+    def encrypt(self, string):
+        string = string.lower()
+        encryptDict = dict(zip(self.originalKey, self.originalDict))
+
+        encryptString = ""
+        for i in string:
+            try:
+                encryptString += encryptDict[i]
+            except KeyError:
+                encryptString += i
+        print(encryptDict)
+        return encryptString
+
+
+class Reflector(object):
+
+    def __init__(self, *args):
+        self.originalDict = [i for i in originalKey]
+        self.__RefId__ = args[0]
+
+        random.shuffle(self.originalDict)
+        self.originalKey = []
+
+        for i in reversed(self.originalDict):
+            self.originalKey.append(i)
+
+    def encrypt(self, string):
+        encryptDict = dict(zip(self.originalKey, self.originalDict))
+
+        encryptString = ""
+        for i in string:
+            try:
+                encryptString += encryptDict[i]
+            except KeyError:
+                encryptString += i
+        print(encryptDict)
+        return encryptString
